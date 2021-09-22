c301e78663c807928578ea2131bbaa4d74816a3a
urgensonamlama <urgensonamlama@gmail.com>
2021-09-22 10:36:27 -0400

    commit tree fc281841ee16fb3e3241856dcb1a9126726a7c30

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Your code here
+function saturdayFun(string="roller-skate"){
+  return ( `This Saturday, I want to ${string}!`);
+}
+//saturdayFun("bathe my dog");
+
+/*function mondayWork(string="go to the office"){
+    return (`This Monday, I will ${string}.`);
+}
+//mondayWork("work from home");
+
+
+*/
+const mondayWork = function(string="go to the office"){
+  
+  return (`This Monday, I will ${string}.`);
+};
+
+
+
+function wrapAdjective(flare="*") {
+    return function (string="special") {
+      return `You are ${flare}${string}${flare}!`
+
+    }   
+    
+}
